5597d4c077bb1ba21ba6120860771bd76b68b474
ahslxjvhapt12 <likjhee1@gmail.com>
2023-01-09 17:57:56 +0900

    Timer
    Add_Timer

---
+++ Assets/Scripts/Others/UI/Timer.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Net.NetworkInformation;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    private float minute = 0;
+    private float second = 0;
+
+    [SerializeField] private bool isRecording = true;
+
+    TextMeshProUGUI timerTxt;
+
+    private void Awake()
+    {
+        timerTxt = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (isRecording)
+        {
+            second += Time.deltaTime;
+            minute = MathF.Floor(second / 60);
+            if (second >= 10)
+            {
+                timerTxt.text = $"0{minute} : {Mathf.Floor(second)}";
+            }
+            else
+            {
+                timerTxt.text = $"0{minute} : 0{Mathf.Floor(second)}";
+            }
+        }
+    }
+
+    [ContextMenu("StartRecord")]
+    /// <summary>
+    /// 타이머를 시작하고 작동 여부를 반환
+    /// </summary>
+    /// <returns></returns>
+    public bool StartRecord()
+    {
+        if (isRecording)
+        {
+            Debug.LogWarning("이미 시간을 기록중입니다!");
+            return false;
+        }
+        else
+        {
+            second = 0;
+            minute = 0;
+            isRecording = true;
+            Debug.Log("기록 시작");
+            return true;
+        }
+    }
+
+    [ContextMenu("StopRecord")]
+    /// <summary>
+    /// 타이머를 멈추고 정지 여부를 반환
+    /// </summary>
+    /// <returns></returns>
+    public bool StopRecord()
+    {
+        if (isRecording == false)
+        {
+            Debug.LogWarning("기록을 하고 있지 않음");
+            return false;
+        }
+        else
+        {
+            isRecording = false;
+            Debug.Log("기록 중지");
+            return true;
+        }
+    }
+}
