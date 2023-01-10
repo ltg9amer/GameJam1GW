2ec57fe18747c4916c00133b86abf0ca3fb1e10f
ahslxjvhapt12 <likjhee1@gmail.com>
2023-01-10 14:24:44 +0900

    Fix : StarCatchStage

--- Assets/Scripts/Others/Stage/StarCatchStage.cs
+++ Assets/Scripts/Others/Stage/StarCatchStage.cs
@@ -2,42 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class StarCatchStage : MonoBehaviour
 {
-    [SerializeField] private bool clickDelay = false;
     Sequence seq;
+
+    [SerializeField] private Image image;
+    private GameObject cam;
 
     bool isTiming = false;
 
     private void OnEnable()
     {
+        cam = Camera.main.gameObject;
         Move();
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && clickDelay == false)
+        if (Input.GetMouseButtonDown(0))
         {
             Debug.Log(1);
-            clickDelay = true;
-            Invoke("DelayChange", 0.2f);
+            if (isTiming == true)
+            {
+                GameManager.instance.GamePlaying();
+            }
+            else
+            {
+                seq.Kill();
+                transform.position = new Vector2(-3, 0);
+                Move();
+                Shake();
+            }
         }
     }
 
-    void DelayChange()
+    private void Shake()
     {
-        clickDelay = false;
-        seq.Kill();
-        transform.position = new Vector2(-3, 0);
-        Move();
+        Sequence sequence = DOTween.Sequence()
+        .Append(image.DOFade(0.7f, 0.1f))
+        .Join(cam.transform.DOShakePosition(0.1f))
+        .Append(image.DOFade(0, 0.1f));
     }
 
     private void Move()
     {
         seq = DOTween.Sequence();
-        seq.Append(transform.DOMove(new Vector2(3, 0), 0.5f).SetEase(Ease.Linear))
-        .Append(transform.DOMove(new Vector2(-3, 0), 0.5f).SetEase(Ease.Linear))
+        seq.Append(transform.DOMove(new Vector2(3, 0), 0.35f).SetEase(Ease.Linear))
+        .Append(transform.DOMove(new Vector2(-3, 0), 0.35f).SetEase(Ease.Linear))
         .OnComplete(() =>
         {
             seq.Kill();
