20c572a252708797798e87a2c349601cabb2475f
ahslxjvhapt12 <likjhee1@gmail.com>
2023-01-11 21:52:34 +0900

    FIx
    TitleColor

--- Assets/Scripts/Others/UI/RainbowText.cs
+++ Assets/Scripts/Others/UI/RainbowText.cs
@@ -16,7 +16,7 @@ public class RainbowText : MonoBehaviour
     void ColorChange()
     {
         Sequence seq = DOTween.Sequence()
-        .Append(text.DOColor(new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)), 1f))
+        .Append(text.DOColor(new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)), 0.5f))
         .OnComplete(() =>
         {
             ColorChange();
