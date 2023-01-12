52f908978ef9fd34f3dfa283dd9c8ca29b905506
ahslxjvhapt12 <likjhee1@gmail.com>
2023-01-12 15:48:53 +0900

    Sound

--- Assets/Scripts/Others/Object/Bubble.cs
+++ Assets/Scripts/Others/Object/Bubble.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
+    AudioSource _audio;
+    
+    private void Awake()
+    {
+        _audio = GetComponent<AudioSource>();    
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Pin")
         {
+            _audio?.Play();
             Destroy(gameObject);
         }
     }
